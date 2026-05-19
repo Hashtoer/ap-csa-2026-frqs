a964a7caaefb765eb540db1a5af0cbe20b797f96
Hashtoer <henryhe510@gmail.com>
2026-05-19 11:22:25 -0400

    Update Solution.java

--- question-3/Solution.java
+++ question-3/Solution.java
@@ -1,1 +1,18 @@
+public int moreHistoryThanMathAbsences() {
+    String name;
+    int count = 0;
 
+    for (int i = 0; i < historyList.size(); i++) {
+        name = historyList.get(i).getStudentID();
+
+        for (int g = 0; g < mathList.size(); g++) {
+            if (mathList.get(g).getStudentID().equals(name)) {
+                if (historyList.get(i).getAbsences() > mathList.get(g).getAbsences()) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
+}
