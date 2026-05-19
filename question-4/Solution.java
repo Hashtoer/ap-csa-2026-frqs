594f7ca51a2e6ec259881dc36d2ee218abdf55c5
Hashtoer <henryhe510@gmail.com>
2026-05-19 11:22:51 -0400

    Update Solution.java

--- question-4/Solution.java
+++ question-4/Solution.java
@@ -1,1 +1,19 @@
+public int getPointsForRow(int targetRow) {
+    int sum = 0;
+    boolean sameColor = true;
+    String color = board[targetRow][0].getColor();
 
+    for (int i = 0; i < board[0].length; i++) {
+        sum += board[targetRow][i].getPoints();
+
+        if (!(board[targetRow][i].getColor().equals(color))) {
+            sameColor = false;
+        }
+    }
+
+    if (sameColor) {
+        return sum * 2;
+    }
+
+    return sum;
+}
