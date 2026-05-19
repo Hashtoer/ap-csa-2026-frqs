5faadde94ab647d9ab5882216ba972a4f4c67120
Hashtoer <henryhe510@gmail.com>
2026-05-19 11:19:12 -0400

    Update Solution.java

--- question-1/Solution.java
+++ question-1/Solution.java
@@ -1,1 +1,22 @@
+//part A
+public Account(String requestedName) {
+    username = requestedName;
+    int j = 1;
 
+    while (!Account.isAvailable(username)) {
+        username = requestedName + j;
+        j++;
+    }
+}
+
+//Part B
+public String getShortenedName() {
+    String ret = username;
+
+    while (ret.indexOf("-") > -1) {
+        int index = ret.indexOf("-");
+        ret = ret.substring(0, index - 1) + ret.substring(index + 1);
+    }
+
+    return ret;
+}
