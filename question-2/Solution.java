2cc181221a3089aae7673c8a97867132d778a971
Hashtoer <henryhe510@gmail.com>
2026-05-19 11:19:49 -0400

    Update Solution.java

--- question-2/Solution.java
+++ question-2/Solution.java
@@ -1,1 +1,17 @@
+public class Bottle{
+    private double capacity;
+    private double current;
 
+    public Bottle(double c){
+        capacity = c;
+        current = c;
+    }
+
+    public double updateAmount(double liquid){
+        current -= liquid;
+        if (current < (capacity * .25)){
+            current = capacity;
+        }
+        return current;
+    }
+}
